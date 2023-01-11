b3ab34088873e72954d5a4b66d5f1575083ac1d8
fabioVV <fabio-varela@protonmail.com>
2023-01-11 20:25:52 -0300

    added msg style

---
+++ static/app.js
@@ -0,0 +1,11 @@
+let a = document.getElementById('msg')
+
+
+setTimeout(() =>{
+    a.classList.add('removed')
+    a.addEventListener("transitionend",() =>{
+        a.remove()
+    })
+}, 1500)
+
+
